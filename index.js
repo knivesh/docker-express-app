ebd351f9a393ffacd763a43122e4fed11207bfe2
knivesh <knivesh.stats@gmail.com>
2025-12-16 14:43:37 +0100

    Changing display message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('<h1>A quick brown fox jumped over the lazy dog!</h1>')
 })
 
 const PORT = 8080
